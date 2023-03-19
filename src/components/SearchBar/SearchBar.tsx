3569d341b12cfcfffce5749d35cd0b06e3b90e4f
Yulia Bondar <yuliabondar98@gmail.com>
2023-03-19 13:49:35 +0300

    feat: save seacrh input value to LocalStorage

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,13 +1,41 @@
 import React from 'react';
+import { Props, SearchState } from 'types/types';
 
 import './SearchBar.css';
 
-function SearchBar() {
-  return (
-    <form id="search-form" role="search">
-      <input aria-label="Search" placeholder="Search" type="search" className="search-input" />
-    </form>
-  );
+class SearchBar extends React.Component<Props, SearchState> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      searchValue: localStorage.getItem('searchValue') || '',
+    };
+  }
+
+  componentDidUpdate(prevState: SearchState) {
+    if (prevState.searchValue !== this.state.searchValue) {
+      localStorage.setItem('searchValue', this.state.searchValue);
+    }
+  }
+
+  handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const searchValue = event.target.value;
+    this.setState({ searchValue });
+  };
+
+  render() {
+    return (
+      <form id="search-form" role="search">
+        <input
+          aria-label="Search"
+          placeholder="Search"
+          type="search"
+          value={this.state.searchValue}
+          onInput={this.handleSearchChange}
+          className="search-input"
+        />
+      </form>
+    );
+  }
 }
 
 export default SearchBar;
