af32abb66e1b8f59e1328720216e372b5fc07775
Yulia Bondar <yuliabondar98@gmail.com>
2023-03-20 22:32:58 +0300

    feat: add vitest

--- src/components/SearchBar/SearchBar.test.tsx
+++ src/components/SearchBar/SearchBar.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { describe, expect, test } from 'vitest';
 import SearchBar from './SearchBar';
 
 describe('Search Component', () => {
