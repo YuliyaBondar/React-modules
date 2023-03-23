95e219be7c95cd0cf334a0aa4ac9b30317efe7dc
Yulia Bondar <yuliabondar98@gmail.com>
2023-03-24 02:03:07 +0300

    feat: add form validation

--- src/routes/form/form.tsx
+++ src/routes/form/form.tsx
@@ -8,6 +8,7 @@ type Props = {};
 class Form extends React.Component<Props> {
   nameInput: React.RefObject<HTMLInputElement>;
   dateInput: React.RefObject<HTMLInputElement>;
+  selectValue: React.RefObject<HTMLSelectElement>;
   fileInput: React.RefObject<HTMLInputElement>;
   checkboxInput: React.RefObject<HTMLInputElement>;
   radioInput: React.RefObject<HTMLInputElement>;
@@ -17,6 +18,7 @@ class Form extends React.Component<Props> {
     this.handleSubmit = this.handleSubmit.bind(this);
     this.nameInput = React.createRef();
     this.dateInput = React.createRef();
+    this.selectValue = React.createRef();
     this.fileInput = React.createRef();
     this.checkboxInput = React.createRef();
     this.radioInput = React.createRef();
@@ -36,18 +38,20 @@ class Form extends React.Component<Props> {
             <form onSubmit={this.handleSubmit} className="form">
               <label>
                 <span>Product name:</span>
-                <input type="text" ref={this.nameInput} className="form__input_text" />
+                <input type="text" ref={this.nameInput} className="form__input_text" required />
               </label>
               <label>
                 <span>Date:</span>
-                <input type="date" ref={this.dateInput} className="form__input_date" />
+                <input type="date" ref={this.dateInput} className="form__input_date" required />
               </label>
               <label>
                 <span>Select:</span>
                 <select
                   name="selectedOption"
                   defaultValue="option_1"
+                  ref={this.selectValue}
                   className="form__input_select"
+                  required
                 >
                   <option value="option_1">Option 1</option>
                   <option value="option_2">Option 2</option>
@@ -56,11 +60,15 @@ class Form extends React.Component<Props> {
               <div className="flex__container">
                 <label>
                   <span>Checkbox:</span>
-                  <input type="checkbox" ref={this.checkboxInput} className="h-mr30" />
+                  <input type="checkbox" ref={this.checkboxInput} className="h-mr30" required />
                 </label>
                 <label>
                   <span>Switcher:</span>
-                  <input type="radio" ref={this.radioInput} />
+                  <input type="radio" name="switcher" ref={this.radioInput} required />
+                </label>
+                <label>
+                  <span>Switcher:</span>
+                  <input type="radio" name="switcher" ref={this.radioInput} />
                 </label>
               </div>
               <label>
@@ -70,6 +78,7 @@ class Form extends React.Component<Props> {
                   accept="image/*"
                   ref={this.fileInput}
                   className="form__input_file"
+                  required
                 />
               </label>
               <input type="submit" value="Submit" className="form__input_submit" />
