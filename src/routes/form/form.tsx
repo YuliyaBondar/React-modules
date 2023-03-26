6dfb257d78ab2a7ea00cfaa1b2f5be4320d6b438
Yulia Bondar <yuliabondar98@gmail.com>
2023-03-26 23:31:54 +0300

    feat: add reset for form

--- src/routes/form/form.tsx
+++ src/routes/form/form.tsx
@@ -13,7 +13,8 @@ class Form extends React.Component<Props, IData[]> {
   categorySelectValue: React.RefObject<HTMLSelectElement>;
   imageFileInput: React.RefObject<HTMLInputElement>;
   isFormelyUsed: React.RefObject<HTMLInputElement>;
-  materialRadioInput: React.RefObject<HTMLInputElement>;
+  materialCottonRadioInput: React.RefObject<HTMLInputElement>;
+  materialDenimRadioInput: React.RefObject<HTMLInputElement>;
 
   constructor(props: Props) {
     super(props);
@@ -23,7 +24,8 @@ class Form extends React.Component<Props, IData[]> {
     this.categorySelectValue = React.createRef();
     this.imageFileInput = React.createRef();
     this.isFormelyUsed = React.createRef();
-    this.materialRadioInput = React.createRef();
+    this.materialCottonRadioInput = React.createRef();
+    this.materialDenimRadioInput = React.createRef();
     this.state = {
       createdCards: JSON.parse(localStorage.getItem('createdCards') || '[]'),
     };
@@ -41,12 +43,15 @@ class Form extends React.Component<Props, IData[]> {
       releaseDate: this.releaseDateInput.current.value,
       image: this.imageFileInput.current.value,
       category: this.categorySelectValue.current.value,
-      isFormelyUsed: this.isFormelyUsed.current.value,
-      material: this.materialRadioInput.current.value,
+      isFormelyUsed: this.isFormelyUsed.current.checked,
+      material: this.materialCottonRadioInput.current.checked
+        ? this.materialCottonRadioInput.current.value
+        : this.materialDenimRadioInput.current.value,
     };
     createdCards.push(createdCard);
     this.setState({ createdCards });
     event.preventDefault();
+    (event.target as HTMLFormElement).reset();
   }
 
   render() {
@@ -91,16 +96,27 @@ class Form extends React.Component<Props, IData[]> {
               </label>
               <div className="flex__container">
                 <label>
-                  <span>Is formely used:</span>
+                  <span className="isFormelyUsed">Is formely used:</span>
                   <input type="checkbox" ref={this.isFormelyUsed} className="h-mr30" required />
                 </label>
                 <label>
                   <span>Хлопок:</span>
-                  <input type="radio" name="switcher" ref={this.materialRadioInput} required />
+                  <input
+                    type="radio"
+                    name="switcher"
+                    value="Хлопок"
+                    ref={this.materialCottonRadioInput}
+                    required
+                  />
                 </label>
                 <label>
                   <span>Деним:</span>
-                  <input type="radio" name="switcher" ref={this.materialRadioInput} />
+                  <input
+                    type="radio"
+                    name="switcher"
+                    value="Деним"
+                    ref={this.materialDenimRadioInput}
+                  />
                 </label>
               </div>
               <label>
