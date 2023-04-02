ff24251d1be83a8e023ad65957c57d877a7208ea
Yulia Bondar <yuliabondar98@gmail.com>
2023-04-03 00:24:54 +0300

    refactor: add hooks to Form

--- src/routes/form/form.tsx
+++ src/routes/form/form.tsx
@@ -1,128 +1,124 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Cards from '../../components/Cards/Cards';
-import { IData } from '../../utils/interfaces';
 
 import './form.css';
 
-type Props = Record<string, never>;
+function Form() {
+  const [productNameInput, setProductNameInput] = useState('');
+  const [releaseDateInput, setReleaseDateInput] = useState('');
+  const [categorySelectValue, setCategorySelectValue] = useState('Футболки');
+  const [imageFileInput, setImageFileInput] = useState('');
+  const [isFormelyUsed, setIsFormelyUsed] = useState(Boolean);
+  const [materialCottonRadioInput, setMaterialCottonRadioInput] = useState('');
+  const [materialDenimRadioInput, setMaterialDenimRadioInput] = useState('');
 
-type SearchState = {
-  createdCards: IData[];
-};
+  const [createdCards, setCreatedCards] = useState(() => {
+    return JSON.parse(localStorage.getItem('createdCards') || '[]');
+  });
 
-class Form extends React.Component<Props, SearchState> {
-  productNameInput: React.RefObject<HTMLInputElement>;
-  releaseDateInput: React.RefObject<HTMLInputElement>;
-  categorySelectValue: React.RefObject<HTMLSelectElement>;
-  imageFileInput: React.RefObject<HTMLInputElement>;
-  isFormelyUsed: React.RefObject<HTMLInputElement>;
-  materialCottonRadioInput: React.RefObject<HTMLInputElement>;
-  materialDenimRadioInput: React.RefObject<HTMLInputElement>;
-
-  constructor(props: Props) {
-    super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.productNameInput = React.createRef();
-    this.releaseDateInput = React.createRef();
-    this.categorySelectValue = React.createRef();
-    this.imageFileInput = React.createRef();
-    this.isFormelyUsed = React.createRef();
-    this.materialCottonRadioInput = React.createRef();
-    this.materialDenimRadioInput = React.createRef();
-    this.state = {
-      createdCards: JSON.parse(localStorage.getItem('createdCards') || '[]'),
-    };
-  }
-
-  componentDidUpdate() {
-    localStorage.setItem('createdCards', JSON.stringify(this.state.createdCards));
-  }
-
-  handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     alert('The data has been saved.');
-    const createdCards = this.state.createdCards;
-    const createdCard = {
-      name: this.productNameInput.current?.value,
-      releaseDate: this.releaseDateInput.current?.value,
-      image: URL.createObjectURL(this.imageFileInput.current!.files![0]),
-      category: this.categorySelectValue.current?.value,
-      isFormelyUsed: this.isFormelyUsed.current?.checked,
-      material: this.materialCottonRadioInput.current?.checked
-        ? this.materialCottonRadioInput.current?.value
-        : this.materialDenimRadioInput.current?.value,
-    };
-    createdCards.push(createdCard);
-    this.setState({ createdCards });
     event.preventDefault();
+    const blob = new Blob([imageFileInput], { type: 'image/*' });
+    setCreatedCards([
+      ...createdCards,
+      {
+        name: productNameInput,
+        releaseDate: releaseDateInput,
+        image: URL.createObjectURL(blob),
+        category: categorySelectValue,
+        isFormelyUsed: isFormelyUsed,
+        material: materialCottonRadioInput ? materialCottonRadioInput : materialDenimRadioInput,
+      },
+    ]);
     (event.target as HTMLFormElement).reset();
-  }
+  };
 
-  render() {
-    return (
-      <div className="form-page">
-        <form onSubmit={this.handleSubmit} className="form" data-testid="form">
+  useEffect(() => {
+    localStorage.setItem('createdCards', JSON.stringify(createdCards));
+  }, [createdCards]);
+
+  console.log(imageFileInput);
+
+  return (
+    <div className="form-page">
+      <form onSubmit={handleSubmit} className="form" data-testid="form">
+        <label>
+          <span>Product name:</span>
+          <input
+            type="text"
+            className="form__input_text"
+            onChange={(e) => setProductNameInput(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          <span>Release date:</span>
+          <input
+            type="date"
+            onChange={(e) => setReleaseDateInput(e.target.value)}
+            className="form__input_date"
+            required
+          />
+        </label>
+        <label>
+          <span>Category:</span>
+          <select
+            name="selectedOption"
+            onChange={(e) => setCategorySelectValue(e.target.value)}
+            className="form__input_select"
+            required={true}
+          >
+            <option value="Футболки">Футболки</option>
+            <option value="Джинсы">Джинсы</option>
+            <option value="Кроссовки">Кроссовки</option>
+          </select>
+        </label>
+        <div className="flex__container">
           <label>
-            <span>Product name:</span>
-            <input type="text" ref={this.productNameInput} className="form__input_text" required />
-          </label>
-          <label>
-            <span>Release date:</span>
-            <input type="date" ref={this.releaseDateInput} className="form__input_date" required />
-          </label>
-          <label>
-            <span>Category:</span>
-            <select
-              name="selectedOption"
-              ref={this.categorySelectValue}
-              className="form__input_select"
-              required
-            >
-              <option value="Футболки">Футболки</option>
-              <option value="Джинсы">Джинсы</option>
-              <option value="Кроссовки">Кроссовки</option>
-            </select>
-          </label>
-          <div className="flex__container">
-            <label>
-              <span className="isFormelyUsed">Is formely used:</span>
-              <input type="checkbox" ref={this.isFormelyUsed} className="h-mr30" required />
-            </label>
-            <label>
-              <span>Хлопок:</span>
-              <input
-                type="radio"
-                name="switcher"
-                value="Хлопок"
-                ref={this.materialCottonRadioInput}
-                required
-              />
-            </label>
-            <label>
-              <span>Деним:</span>
-              <input
-                type="radio"
-                name="switcher"
-                value="Деним"
-                ref={this.materialDenimRadioInput}
-              />
-            </label>
-          </div>
-          <label>
-            <span>Upload file:</span>
+            <span className="isFormelyUsed">Is formely used:</span>
             <input
-              type="file"
-              accept="image/*"
-              ref={this.imageFileInput}
-              className="form__input_file"
+              type="checkbox"
+              onChange={(e) => setIsFormelyUsed(e.target.checked)}
+              className="h-mr30"
               required
             />
           </label>
-          <input type="submit" value="Submit" className="form__input_submit" />
-        </form>
-        <Cards cards={this.state.createdCards} />
-      </div>
-    );
-  }
+          <label>
+            <span>Хлопок:</span>
+            <input
+              type="radio"
+              name="switcher"
+              value="Хлопок"
+              onChange={(e) => setMaterialCottonRadioInput(e.target.value)}
+              required
+            />
+          </label>
+          <label>
+            <span>Деним:</span>
+            <input
+              type="radio"
+              name="switcher"
+              value="Деним"
+              onChange={(e) => setMaterialDenimRadioInput(e.target.value)}
+            />
+          </label>
+        </div>
+        <label>
+          <span>Upload file:</span>
+          <input
+            type="file"
+            accept="image/*"
+            onChange={(e) => setImageFileInput(e.target.value)}
+            className="form__input_file"
+            required
+          />
+        </label>
+        <input type="submit" value="Submit" className="form__input_submit" />
+      </form>
+      <Cards cards={createdCards} />
+    </div>
+  );
 }
 
 export default Form;
