35c6494d87f28ca385f2b8734192f8e80c5f5eb1
Yulia Bondar <yuliabondar98@gmail.com>
2023-04-10 02:02:28 +0300

    feat: add loading spinner

--- src/components/CardsOnMain/CardsOnMain.tsx
+++ src/components/CardsOnMain/CardsOnMain.tsx
@@ -47,9 +47,13 @@ function CardsOnMain() {
   };
 
   if (error) {
-    return <div>Ошибка: {error.message}</div>;
+    return <div>Error: {error.message}</div>;
   } else if (!isLoaded) {
-    return <div>Загрузка...</div>;
+    return (
+      <div className="loader-container">
+        <div className="spinner"></div>
+      </div>
+    );
   } else {
     return (
       <>
